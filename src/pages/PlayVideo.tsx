1b1ce02f8b51d500225742dd6781f0259b3691f0
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2026-08-06 03:12:41 +0800

    Update random video URL in PlayVideo component

--- src/pages/PlayVideo.tsx
+++ src/pages/PlayVideo.tsx
@@ -93,7 +93,7 @@ export function PlayVideo() {
   const playerInstance = useRef<any>(null);
 
   const randomUrls = [
-    'https://www.effectivecpmnetwork.com/b3c368xkxz?key=430d12c7fd58ecba841fd905ca2dd3fd',
+    'https://viiukuhe.com/dc/?blockID=427841',
     'https://dulyhagglermounting.com/2082665'
   ];
 
